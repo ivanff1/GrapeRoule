90dfdaf4b97a8038ddfdee01a0297ff4e4ce6add
Ivan Zlatanov <ivanff1@abv.bg>
2018-06-28 17:43:53 +0300

    added keyGenerator

---
+++ GrapeRoule/UserKeyGenerator/Unique_User_Key_Generation/Base64Convert.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Unique_User_Key_Generation
+{
+    class Base64Convert
+    {
+        public static string EncodeData(string incomingString)
+        {
+            byte[] baseBytes = Encoding.UTF8.GetBytes(incomingString);
+            string baseString = Convert.ToBase64String(baseBytes);
+
+            return baseString;
+        }
+    }
+}
---
+++ GrapeRoule/UserKeyGenerator/Unique_User_Key_Generation/GenerateKey.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Unique_User_Key_Generation
+{
+    public class GenerateKey
+    {
+        public static string Generate(long userID, string username)
+        {
+            string subJump0 = "";
+            long usernameValue = 0;
+            string usernameBaseValue = Base64Convert.EncodeData(username);
+
+            for (int i = 0; i < username.Length; i++)
+            {
+                usernameValue += (byte)username[i];
+            }
+
+            for (int j = 0; j < usernameBaseValue.Length; j++)
+            {
+                subJump0 += (char)(usernameBaseValue[j] + (usernameValue/userID));
+            }
+            string generatedKey = Base64Convert.EncodeData(subJump0);            
+            return generatedKey;
+        }
+    }
+}
